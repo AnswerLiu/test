07d0cac3ffc42780339e70d6286b61922c69fb62
Answer <lp120618528@163.com>
2016-11-28 11:38:48 +0800

    update readme.txt and test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: anchen
+# @Date:   2016-11-28 11:28:20
+# @Last Modified by:   anchen
+# @Last Modified time: 2016-11-28 11:37:47
+print(123)
+
+print(1+2)
